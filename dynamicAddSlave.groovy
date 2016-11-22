d3cc4a1275fe3e1cf89b9bf2b6def89b40d2573f
metacoma <sryabin@mirantis.com>
2016-11-22 12:10:20 +0400

    random port

--- dynamicAddSlave.groovy
+++ dynamicAddSlave.groovy
@@ -7,9 +7,13 @@ import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
 
 def publicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDt52BavE41oHG25pIZyrbjB+o8ipuceNI5fd7DkdH5e/XOO6EfScRhkmavkFJvLB6Lf1HOzL2bR6ApP9JvCZ69ZiXueGLrQHImJVjBPFy9S7k7u4eEC5I1rSdfu8pE4QgmbR5XFjLd7Ox9X3yLg5FkxU8r9mpE9QquPVHHfhDMsEaa2miDVHylocXglqNakAuHmogva/PP+mRs4XLiAG4o7z94B9jvqADU549/SuYUpgZ5uV4M8n1CXmBHT8vIaQXYYt2l5nKxHTNtZSztJ9eOqlOHxN4f5O88z6zeVp33/EPeH2oyy/CF4r4QegrHW56OODRXlM3b2bNrQf93Tsjf"
 
+Random random = new Random();
 def randomPort() {
-    // TODO generate random port and check its via netstat -nlp
-    return 4423
+    
+    def minPort = 1025
+    def maxPort = 65534
+    /* TODO check port is free */
+    return random.nextInt(maxPort - minPort) + minPort;
 }
 
 def slavePort = randomPort()
